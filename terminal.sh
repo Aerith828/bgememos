f6e7289625a625c3fb7bcbb157c985e990d7239b
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-03-19 10:24:08 +0100

    Modif2

--- terminal.sh
+++ terminal.sh
@@ -9,7 +9,8 @@ git commit -m "<message à ajouter au commit>"   # Sauvegarde les modifications 
 git status                                      # affiche l'état de mon projet Git à cet instant
 git log                                         # affiche l'historique des commits
 git restore <nom du fichier/dossier>            # remet le contenu du fichier à l'état du dernier commit
-git checkout <numéro du commit>                 # Permet de nous déplacer dans le temps, dans des dimensions différentes
+git checkout <numéro du commit/hash>            # Permet de nous déplacer dans le temps, dans des dimensions différentes
 git checkout <master>                           # retourne au dernier commit
 git branch <nom de la branche>                  # créer une nouvelle branche
 git branch                                      # vérifie l'existence d'une branche
+git checkout <nom de la branche>                # dernier commit de la branche en question
