1089b5a62b1bf4987b6e79969318a2328c51d36c
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-03-19 09:48:19 +0100

    add git restore memo

--- terminal.sh
+++ terminal.sh
@@ -8,3 +8,4 @@ git add <nom du fichier/dossier>                # ajouter les modifications au p
 git commit -m "<message à ajouter au commit>"   # Sauvegarde les modifications précedemment ajouter
 git status                                      # affiche l'état de mon projet Git à cet instant
 git log                                         # affiche l'historique des commits
+git restore <nom du fichier/dossier>            # remet le contenu du fichier à l'état du dernier commit
