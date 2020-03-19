df44395add4fcc26fad797ed460ee4d06f6cafba
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-03-19 09:37:47 +0100

    add terminal.sh

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hello world!');
+
+let variable2=2;
+
+variable2="J'ai changé de contenu";
+
+const variable3=3;
+
+console.log("variable 2 : ", variable2);
+console.log("variable 3 : ", variable3);
---
+++ terminal.sh
@@ -0,0 +1,10 @@
+#DIVERS
+cd <chemin du fichier> # se déplacer
+node <nom du fichier> # lancer un script node
+
+#GIT
+git init                                        # initialiser un projet git
+git add <nom du fichier/dossier>                # ajouter les modifications au prochain commit
+git commit -m "<message à ajouter au commit>"   # Sauvegarde les modifications précedemment ajouter
+git status                                      # affiche l'état de mon projet Git à cet instant
+git log                                         # affiche l'historique des commits
