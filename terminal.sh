9a76c8b5450537b8dd84a27c2c4275c6c89e2feb
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-04-09 16:20:09 +0200

    add tableau.js and update variable.js

--- terminal.sh
+++ terminal.sh
@@ -8,12 +8,14 @@ git add <nom du fichier/dossier>                        # ajouter les modificati
 git commit -m "<message à ajouter au commit>"           # Sauvegarde les modifications précedemment ajouter
 git status                                              # affiche l'état de mon projet Git à cet instant
 git log                                                 # affiche l'historique des commits
+git log --pretty=oneline --graph                        # affiche l'historique des commits sous forme de graphique 
 git restore <nom du fichier/dossier>                    # remet le contenu du fichier à l'état du dernier commit
 git checkout <numéro du commit/hash>                    # Permet de nous déplacer dans le temps, dans des dimensions différentes, déplace la tête de lecture
 git checkout <master>                                   # retourne au dernier commit
+git checkout <nom de la branche>                        # dernier commit de la branche en question
 git branch <nom de la branche>                          # créer une nouvelle branche
 git branch                                              # vérifie l'existence d'une branche
-git checkout <nom de la branche>                        # dernier commit de la branche en question
+git branch <nom de la branche> <nom de la branche à copier> # copie la branche désigner dans une nouvelle branche
 git merge  <nom de la branche>                          # fusionne la branche nommé à l'intérieur de la branche actuelle en créant un commit
 git merge <nom de la branche> -m "<message du commit>"  # même chose que la ligne précédente en fonction de l'IDE
 git remote                                              # Liste les dépôt distants de notre projet git
@@ -21,3 +23,5 @@ git remote add <nom du remote> <adresse du remote>      # ajoute un nouveau dép
 git push <nom du dépôt> <branche distante>              # envoie la branche courante sur la branche distante du dépôt
 git push -u <nom du dépôt> < branche distante>          # idem mais paramètre la branche locale pour correspondre à la branche distante de façon permanente
 git clone <nom du dépôt> <nom du dossier>               # Télécharge la branche par défaut du dépôt choisi dans le dossier voulu
+git pull <nom du dépôt> <brnahce distante>              # met à jour la branche actuelle avec les modifications effectuées sur la branche distante
+git push <nom du dépôt> <branche distante>
